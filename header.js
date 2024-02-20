9b4a597d432d2abf1298634214cdd1e6a4eec725
aimanaimey <157308773+aimanaimey@users.noreply.github.com>
2024-02-20 18:48:58 +0800

    Update header.js

--- header.js
+++ header.js
@@ -2,7 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const mobileMenuIcon = document.querySelector('.main-menu-mobile');
     const navLinks = document.querySelector('.right-section');
 
+    // Toggle the 'show' class when the menu button is clicked
     mobileMenuIcon.addEventListener('click', function () {
         navLinks.classList.toggle('show');
+    });
+
+    // Close the dropdown when clicking anywhere on the screen
+    document.addEventListener('click', function (event) {
+        const isClickInsideMenu = navLinks.contains(event.target);
+        const isClickOnMenuIcon = mobileMenuIcon.contains(event.target);
+
+        if (!isClickInsideMenu && !isClickOnMenuIcon) {
+            navLinks.classList.remove('show');
+        }
     });
 });
